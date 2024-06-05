30028829a0d9f1a20ad41f32ee771da7b10e543c
Ahmedmagdi122 <97955901+Ahmedmagdi122@users.noreply.github.com>
2024-06-05 14:51:01 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ from pyromod import listen
 
 bot = Client(
     "simo",
-    api_id=26022994,
-    api_hash="2e84a6b68bd6b5a79f46e8192668e0ea",
-    bot_token="6013874411:AAF95252Q7i6sqOTpDEkBKAca949CxIvIJU",
+    api_id=29621516,
+    api_hash="fa21d1a59b76b215cb366771255a13bf",
+    bot_token="7335504991:AAHuIAqWGeFjtk6qe1TV5QQ8K-2XtHfxj4g",
     plugins=dict(root="Maker")
     )
 
@@ -15,6 +15,6 @@ async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
     await bot.start()
     R_R_B0 = "R_R_B0"
-    await bot.send_message(R_R_B0, "** فشغيل الصانع عزيزي فينوم. **")
-    print("[INFO]: فينوم قلبايي")
+    await bot.send_message(WT_F33, "** فشغيل الصانع عزيزي ديلر. **")
+    print("[INFO]: ديــلــر قلبايي")
     await idle()
